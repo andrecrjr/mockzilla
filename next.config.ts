05fcaaf760d36c679527e85ffa062875ea5bdd66
André Carlos Jr. <andre.junior@globalhitss.com.br>
2025-11-06 12:06:22 -0300

    feat: add CORS headers to API routes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+   async headers() {
+    return [
+      {
+        source: '/api/:path*', // Aplica-se a todas as rotas de API
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: '*' },
+          { key: 'Access-Control-Allow-Headers', value: '*' },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
