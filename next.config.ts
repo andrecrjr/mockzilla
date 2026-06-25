ddf5057906d198bb01548ab434b8d94a461c0387
Andre Carlos Jr <andre-carlos@live.com>
2026-06-24 23:07:52 -0300

    fix: add origins into env

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
 	output: process.env.NODE_ENV === 'development' ? undefined : 'standalone',
 	serverExternalPackages: ['pg', '@electric-sql/pglite', 'handlebars'],
-	allowedDevOrigins: ['192.168.1.20'],
+	allowedDevOrigins: process.env.NODE_ORIGINS?.split(',').map((origin) => origin.trim()) || [],
 	async rewrites() {
 		return [
 			{
